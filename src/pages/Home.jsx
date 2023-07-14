f0f8bf6ff44fcd2893e063ad77b7476c3cde8593
Hafiz Hisham <hafizhisham304@gmail.com>
2023-07-15 00:13:34 +0800

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
-import FolderImage from "./images/file1.png"
+import FolderImage from "/images/file1.png"
 
 const Home = () => {
   const navigate = useNavigate();
